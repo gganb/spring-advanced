8fd2cea871611ef9647f9015a7226fad56c175f9
gganb <gksql0801@naver.com>
2025-04-18 16:45:54 +0900

    Feat : 레벨 1-3 코드 개선 퀴즈 - Validation 적용 - 최소값 8, 정규표현식 적용

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,9 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,10 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
-    @NotBlank
+
+    @NotNull(message = "새 비밀번호는 필수 입력 값 입니다.")
+    @Size(min = 8, message = "새 비밀번호는 8자 이상이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Z])[0-9a-zA-Z]+$", message = "숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
 }
+
