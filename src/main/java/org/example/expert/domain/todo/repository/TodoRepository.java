d333ac31e0619355cf67b6458d92f1f9e39444a9
gganb <gksql0801@naver.com>
2025-04-21 19:18:20 +0900

    Refactor (todo) : default 메서드 생성 - Todo 반환 default 메서드 생성

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,22 +1,28 @@
 package org.example.expert.domain.todo.repository;
 
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-   @EntityGraph(attributePaths = {"user"})
+    @EntityGraph(attributePaths = {"user"})
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
     @EntityGraph(attributePaths = {"user"})
     Optional<Todo> findById(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
+
+    default Todo findByIdOrElseThrow(long todoId) {
+        return findById(todoId).orElseThrow(
+                () -> new InvalidRequestException("Todo not found")
+        );
+    }
 }
