a9ea37a02820be3002dbffcad183498109ffa693
gganb <gksql0801@naver.com>
2025-04-21 18:27:09 +0900

    Refactor(user) : 정적 팩토리메서드 주석 추가, 비밀번호 변경 로직 수정 - 비밀번호 값 검증 로직을 서비스에서 엔티티로 넘겨서 도메인 중심 설계를 하도록 변경함

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -4,13 +4,15 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.expert.config.PasswordEncoder;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.entity.Timestamped;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.enums.UserRole;
 
 @Getter
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 생성자 레벨을 protected로 설정해 객체 생성을 막음
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 생성자 레벨을 protected 설정해 객체 생성을 막음
 @Table(name = "users")
 public class User extends Timestamped {
 
@@ -35,6 +37,14 @@ public class User extends Timestamped {
         this.userRole = userRole;
     }
 
+    /**
+     * AuthService 에서 User 생성시 사용
+     *
+     * @param email
+     * @param encodedPassword
+     * @param userRole
+     * @return
+     */
     public static User of(String email, String encodedPassword, UserRole userRole) {
         return new User(email, encodedPassword, userRole);
     }
@@ -43,11 +53,19 @@ public class User extends Timestamped {
         return new User(authUser.getId(), authUser.getEmail(), authUser.getUserRole());
     }
 
-    public void changePassword(String password) {
-        this.password = password;
+    public void changePassword(String newPassword, String oldPassword, PasswordEncoder passwordEncoder) {
+        if (!passwordEncoder.matches(oldPassword, this.password)) {
+            throw new InvalidRequestException("잘못된 비밀번호입니다.");
+        }
+
+        if (passwordEncoder.matches(newPassword, this.password)) {
+            throw new InvalidRequestException("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.");
+        }
+        this.password = passwordEncoder.encode(newPassword);
     }
 
     public void updateRole(UserRole userRole) {
         this.userRole = userRole;
     }
+
 }
