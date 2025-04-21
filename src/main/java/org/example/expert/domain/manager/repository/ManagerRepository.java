99d313fef047be7c97d3a4353e6c1147c237b2f6
gganb <gksql0801@naver.com>
2025-04-21 21:06:41 +0900

    Refactor(manager) : default 메서드 설정 - findByIdOrElseThrow 메서드 설정해 중복 코드 제거

--- src/main/java/org/example/expert/domain/manager/repository/ManagerRepository.java
+++ src/main/java/org/example/expert/domain/manager/repository/ManagerRepository.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.manager.repository;
 
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.manager.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +11,11 @@ import java.util.List;
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
     @Query("SELECT m FROM Manager m JOIN FETCH m.user WHERE m.todo.id = :todoId")
     List<Manager> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+    default Manager findByIdOrElseThrow(long managerId) {
+        return findById(managerId).orElseThrow(
+                () -> new InvalidRequestException("Manager not found")
+        );
+    }
 }
+
