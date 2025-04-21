aeb69cd631381f5e7dbf93b8f9b6e72bcaaccdbe
gganb <gksql0801@naver.com>
2025-04-21 18:29:12 +0900

    Feat (user) : default 메서드 추가 - 서비스에서 id로 user를 찾고 없으면 예외를 발생시키고 있다면 user를 반환하는 메서드를 레포지토리의 default 메서드로 빼냄

--- src/main/java/org/example/expert/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/expert/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package org.example.expert.domain.user.repository;
 
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,5 +8,13 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
+
+    default User findByIdOrElseThrow(long userId) {
+        return findById(userId).orElseThrow(
+                () -> new InvalidRequestException("User not found")
+        );
+    }
+
 }
