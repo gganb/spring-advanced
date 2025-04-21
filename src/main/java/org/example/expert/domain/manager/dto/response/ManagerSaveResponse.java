db13dd1b541c107989d746466b083fec082a0b82
gganb <gksql0801@naver.com>
2025-04-21 21:10:25 +0900

    Refactor(manager) : 정적 팩토리메서드 구현 - id와 user를 받아와 내부에서 ManagerSaveResponse 생성

--- src/main/java/org/example/expert/domain/manager/dto/response/ManagerSaveResponse.java
+++ src/main/java/org/example/expert/domain/manager/dto/response/ManagerSaveResponse.java
@@ -2,6 +2,7 @@ package org.example.expert.domain.manager.dto.response;
 
 import lombok.Getter;
 import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.entity.User;
 
 @Getter
 public class ManagerSaveResponse {
@@ -12,5 +13,11 @@ public class ManagerSaveResponse {
     public ManagerSaveResponse(Long id, UserResponse user) {
         this.id = id;
         this.user = user;
+    }
+
+    public static ManagerSaveResponse from(long id, User user){
+        return new ManagerSaveResponse(id,
+                new UserResponse(user.getId(),user.getEmail())
+                );
     }
 }
