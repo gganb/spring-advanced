7c1552b1bf8c6ff5fb001977570d6887bfedb145
gganb <gksql0801@naver.com>
2025-04-21 21:10:51 +0900

    Refactor(manager) : 정적 팩토리메서드 구현 - Manager를 받아와 내부에서 ManagerResponse 생성

--- src/main/java/org/example/expert/domain/manager/dto/response/ManagerResponse.java
+++ src/main/java/org/example/expert/domain/manager/dto/response/ManagerResponse.java
@@ -1,7 +1,9 @@
 package org.example.expert.domain.manager.dto.response;
 
 import lombok.Getter;
+import org.example.expert.domain.manager.entity.Manager;
 import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.entity.User;
 
 @Getter
 public class ManagerResponse {
@@ -12,5 +14,13 @@ public class ManagerResponse {
     public ManagerResponse(Long id, UserResponse user) {
         this.id = id;
         this.user = user;
+    }
+
+    public static ManagerResponse fromManagerWithUser(Manager manager){
+        User user = manager.getUser();
+        return  new ManagerResponse(
+                manager.getId(),
+                new UserResponse(user.getId(), user.getEmail())
+        );
     }
 }
