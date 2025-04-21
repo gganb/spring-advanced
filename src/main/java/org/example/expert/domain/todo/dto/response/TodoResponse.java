02f1582686ce5bcf9b95785b0266e62c6ba867b0
gganb <gksql0801@naver.com>
2025-04-21 19:17:48 +0900

    Refactor (todo) : 정적 팩토리메서드 적용 - fromTodoWithUser : todo, user 를 넘겨 TodoResponseDto 변환 - fromTodo : todo를 TodoResponseDto로 변환

--- src/main/java/org/example/expert/domain/todo/dto/response/TodoResponse.java
+++ src/main/java/org/example/expert/domain/todo/dto/response/TodoResponse.java
@@ -2,7 +2,9 @@ package org.example.expert.domain.todo.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.entity.User;
 
 import java.time.LocalDateTime;
 
@@ -18,4 +20,27 @@ public class TodoResponse {
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
+    public static TodoResponse fromTodoWithUser(Todo todo, User user) {
+        return new TodoResponse(
+                todo.getId(),
+                todo.getTitle(),
+                todo.getContents(),
+                todo.getWeather(),
+                new UserResponse(user.getId(), user.getEmail()),
+                todo.getCreatedAt(),
+                todo.getModifiedAt()
+        );
+    }
+
+    public static TodoResponse fromTodo(Todo todo) {
+        return new TodoResponse(
+                todo.getId(),
+                todo.getTitle(),
+                todo.getContents(),
+                todo.getWeather(),
+                new UserResponse(todo.getUser().getId(), todo.getUser().getEmail()),
+                todo.getCreatedAt(),
+                todo.getModifiedAt()
+        );
+    }
 }
